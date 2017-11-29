b176b471debd93e8d56bb7045a7d848886e604ea
Eli Simmonds <esimmond@blue.cs.sonoma.edu>
2017-11-28 18:28:00 -0800

    added all functions

--- SyntacticalAnalyzer.cpp
+++ SyntacticalAnalyzer.cpp
@@ -103,8 +103,41 @@ int SyntacticalAnalyzer::Define ()
 		lex->ReportError ("Expecting 'define'; saw " + lex->GetLexeme ());
 		token = lex->GetToken ();
 	}
-	//else
-		token = lex->GetToken ();
+    token = lex->GetToken ();
+    while (token != LPAREN_T && token != EOF_T)
+    {
+        errors++;
+        lex->ReportError ("Expecting '('; saw " + lex->GetLexeme ());
+        token = lex->GetToken ();
+    }
+    token = lex->GetToken ();
+    while (token != IDENT_T && token != EOF_T)
+    {
+        errors++;
+        lex->ReportError ("Expecting 'IDENT_T'; saw " + lex->GetLexeme ());
+        token = lex->GetToken ();
+    }
+    
+    errors += Param_List();
+    
+    while (token != RPAREN_T && token != EOF_T)
+    {
+        errors++;
+        lex->ReportError ("Expecting ')'; saw " + lex->GetLexeme ());
+        token = lex->GetToken ();
+    }
+    
+    errors += Statement();
+    errors += Statement_List();
+    
+    while (token != RPAREN_T && token != EOF_T)
+    {
+        errors++;
+        lex->ReportError ("Expecting ')'; saw " + lex->GetLexeme ());
+        token = lex->GetToken (); // should we get this?
+    }
+    
+    
 	// token should be in follows of Define
 	p2file << "Exiting Define function; current token is: "
 					<< lex->GetTokenName (token) << endl;
@@ -115,71 +148,122 @@ int SyntacticalAnalyzer::Define ()
 int SyntacticalAnalyzer::More_Defines () {
     int errors = 0;
     token = lex->GetToken();
-    if (token != DEFINE_T) {
-        return errors;
+    if (token == EOF_T) {
+        return errors; // lambda
     }
+    // has to be this becaus it is not lambda
     errors += Define();
-    return errors + More_Defines();
+    errors += More_Defines();
+    return errors;
 }
 
 int SyntacticalAnalyzer::Param_List () {
+    int errors = 0;
     token = lex->GetToken();
-    if (token != IDENT_T) { // null check
-        return 0;
+    if (token == IDENT_T) {
+        return Param_List();
     }
-    return Param_List();
+    else if (token == RPAREN_T) {
+        return errors; // lambda
+    }
+    errors++;
+    lex->ReportError ("Expecting ')'; saw " + lex->GetLexeme ());
+    return errors;
 }
 
 int SyntacticalAnalyzer::Statement () {
     int errors = 0;
     token = lex->GetToken();
     if (token == IDENT_T) {
-        return 0;
+        return errors;
     }
     else if(token == LPAREN_T) {
-        // call action
-    }
-    else {
-        errors += Literal();
+        errors += Action();
+        while (token != RPAREN_T && token != EOF_T) {
+            errors++;
+            lex->ReportError ("Expecting ')'; saw " + lex->GetLexeme ());
+            token = lex->GetToken (); // should we get this?
+        }
+        return errors;
     }
     
+    errors += Literal();
+    
+    return errors;
+}
+
+int SyntacticalAnalyzer::Statement_List () {
+    int errors = 0;
+    token = lex->GetToken();
+    if (token == RPAREN_T) { // check for lambda
+        return errors;
+    }
+    errors += Statement();
+    errors += Statement_List();
+    return errors;
+}
+
+int SyntacticalAnalyzer::Statement_Pair () {
+    int errors = 0;
+    token = lex->GetToken();
+    if (token == LPAREN_T) {
+        errors += Statement_Pair_Body();
+        token = lex->GetToken();
+        if (token == RPAREN_T) {
+            return errors;
+        }
+        else {
+            errors++;
+            lex->ReportError ("Expecting ')'; saw " + lex->GetLexeme ());
+            return errors;
+        }
+    }
+    errors++;
+    lex->ReportError ("Expecting '('; saw " + lex->GetLexeme ());
+    return errors;
+}
+
+int SyntacticalAnalyzer::Statement_Pair_Body () {
+    int errors = 0;
+    token = lex->GetToken();
+    if (token == LPAREN_T) {
+        errors += Action();
+        token = lex->GetToken();
+        if (token != RPAREN_T) {
+            errors++;
+            lex->ReportError ("Expecting ')'; saw " + lex->GetLexeme ());
+            return errors;
+        }
+        token = lex->GetToken();
+        errors += Statement();
+        return errors;
+    }
+    else if (token == NUMLIT_T || token == STRLIT_T || token == QUOTE_T) {
+        errors += Literal();  // token will be an issue that we already have it.
+        return errors;
+    }
+    errors += Action();
     return errors;
 }
 
 int SyntacticalAnalyzer::Literal () {
     int errors = 0;
-    token = lex->GetToken();
+    token = lex->GetToken(); // should we be getting this token before
     if (token == NUMLIT_T || token == STRLIT_T) {
-        return 0;
+        return errors;
     }
     else if (QUOTE_T) {
         errors += Quoted_Lit();
     }
-    else {
-        return errors++;
-    }
+    errors++;
+    lex->ReportError ("Expecting 'LITERAL'; saw " + lex->GetLexeme ());
+    return errors;
 }
 
 int SyntacticalAnalyzer::Quoted_Lit () {
     int errors = 0;
-    token = lex->GetToken();
-    // errors += Any_Other_Token();
+    errors += Any_Other_Token();
     return errors;
-}
-
-int SyntacticalAnalyzer::Any_Other_Token () {
-    int errors = 0;
-    token = lex->GetToken();
-    if (token == LPAREN_T) {
-        // more tokens and rparent_t
-    }
-    else if (token == QUOTE_T) {
-        errors += Any_Other_Token();
-    }
-    else if (token > 0 && token != RPAREN_T && token != LAMBDA && token != EOF_T) {
-        return errors;
-    }
-    return ++errors;
 }
 
 int SyntacticalAnalyzer::More_Tokens () {
@@ -191,4 +275,172 @@ int SyntacticalAnalyzer::More_Tokens () {
     errors += Any_Other_Token();
     return errors + More_Tokens();
 }
+
+int SyntacticalAnalyzer::Any_Other_Token () {
+    int errors = 0;
+    token = lex->GetToken();
+    switch (token) {
+        case LPAREN_T:
+            errors += More_Tokens();
+            token = lex->GetToken();
+            if (token != RPAREN_T) {
+                errors++;
+                lex->ReportError ("Expecting ')'; saw " + lex->GetLexeme ());
+            }
+            return errors;
+        case IDENT_T:   return errors;
+        case NUMLIT_T:  return errors;
+        case STRLIT_T:  return errors;
+        case CONS_T:  return errors;
+        case IF_T:  return errors;
+        case DISPLAY_T:   return errors;
+        case NEWLINE_T:  return errors;
+        case LISTOP_T:  return errors;
+        case AND_T:  return errors;
+        case OR_T:  return errors;
+        case NOT_T:   return errors;
+        case DEFINE_T:  return errors;
+        case NUMBERP_T:  return errors;
+        case SYMBOLP_T:  return errors;
+        case LISTP_T:  return errors;
+        case ZEROP_T:   return errors;
+        case NULLP_T:  return errors;
+        case STRINGP_T:  return errors;
+        case PLUS_T:  return errors;
+        case MINUS_T:  return errors;
+        case DIV_T:   return errors;
+        case MULT_T:  return errors;
+        case MODULO_T:  return errors;
+        case EQUALTO_T:  return errors;
+        case GT_T:  return errors;
+        case LT_T:   return errors;
+        case GTE_T:  return errors;
+        case LTE_T:  return errors;
+        case QUOTE_T:
+            errors += Any_Other_Token();
+            return errors;
+        default:
+            errors++;
+            lex->ReportError ("Expecting TOKEN; saw " + lex->GetLexeme ());
+    }
+}
+
+int SyntacticalAnalyzer::Action () {
+    int errors = 0;
+    token = lex->GetToken();
+    switch (token) {
+        case IF_T:
+            errors += Statement();
+            errors += Statement();
+            errors += Else_Part();
+            return errors;
+        case COND_T:
+            errors += Statement_Pair();
+            errors += More_Pairs();
+            return errors;
+        case LISTOP_T:
+            errors += Statement();
+            return errors;
+        case CONS_T:
+            errors += Statement();
+            errors += Statement();
+            return errors;
+        case AND_T:
+            errors += Statement_List();
+            return errors;
+        case OR_T:
+            errors += Statement_List();
+            return errors;
+        case NOT_T:
+            errors += Statement();
+            return errors;
+        case NUMBERP_T:
+            errors += Statement();
+            return errors;
+        case SYMBOLP_T:
+            errors += Statement();
+            return errors;
+        case LISTP_T:
+            errors += Statement();
+            return errors;
+        case ZEROP_T:
+            errors += Statement();
+            return errors;
+        case NULLP_T:
+            errors += Statement();
+            return errors;
+        case STRINGP_T:
+            errors += Statement();
+            return errors;
+        case PLUS_T:
+            errors += Statement_List();
+            return errors;
+        case MINUS_T:
+            errors += Statement();
+            errors += Statement_List();
+            return errors;
+        case DIV_T:
+            errors += Statement();
+            errors += Statement_List();
+            return errors;
+        case MULT_T:
+            errors += Statement_List();
+            return errors;
+        case MODULO_T:
+            errors += Statement();
+            errors += Statement();
+            return errors;
+        case EQUALTO_T:
+            errors += Statement_List();
+            return errors;
+        case GT_T:
+            errors += Statement_List();
+            return errors;
+        case LT_T:
+            errors += Statement_List();
+            return errors;
+        case GTE_T:
+            errors += Statement_List();
+            return errors;
+        case LTE_T:
+            errors += Statement_List();
+            return errors;
+        case IDENT_T:
+            errors += Statement_List();
+            return errors;
+        case DISPLAY_T:
+            errors += Statement();
+            return errors;
+        case NEWLINE_T:
+            return errors;
+        default:
+            errors++;
+            lex->ReportError ("Expecting 'ACTION'; saw " + lex->GetLexeme ());
+            return errors;
+    }
+}
+
+int SyntacticalAnalyzer::Else_Part() {
+    int errors = 0;
+    token = lex->GetToken();
+    if (token == RPAREN_T) {
+        return errors;
+    }
+    errors += Statement();
+    return errors;
+}
+
+int SyntacticalAnalyzer::More_Pairs() {
+    int errors = 0;
+    token = lex->GetToken();
+    if (token == RPAREN_T) {
+        return errors; // lambda
+    }
+    errors += Statement_Pair();
+    errors += More_Pairs();
+    return errors;
+}
+
+
+
 
--- SyntacticalAnalyzer.h
+++ SyntacticalAnalyzer.h
@@ -25,15 +25,21 @@ class SyntacticalAnalyzer
 	LexicalAnalyzer * lex;
 	ofstream p2file;
 	token_type token;
-	int Program ();
-    int Define ();
-    int More_Defines ();
-    int Param_List ();
-    int Statement ();
-    int Literal ();
-    int Quoted_Lit ();
-    int Any_Other_Token ();
+	int Program (); // done
+    int Define (); // done
+    int More_Defines (); // done
+    int Param_List (); //done
+    int Statement (); // done
+    int Statement_List (); // done
+    int Statement_Pair (); // done
+    int Statement_Pair_Body (); // check for issue with Literal
+    int Literal ();  // done
+    int Quoted_Lit (); // done
+    int Any_Other_Token (); //
     int More_Tokens ();
+    int Action (); // done
+    int Else_Part (); // done
+    int More_Pairs ();
 };
 	
 #endif
